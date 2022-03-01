7e31b06d86efc286d7f6825c94b380a7dd5ace01
FGPortales <fportalesar@gmail.com>
2022-02-28 22:52:40 -0500

    [IMP] Added new scripts and new features.

--- Scripts01/contador_ratings.py
+++ Scripts01/contador_ratings.py
@@ -4,7 +4,7 @@ import collections
 conf = SparkConf().setMaster('local').setAppName('RatingsHistogram')
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile('file:///C:/Users/freddygpa/Documents/Courses/pyspark/ml-100k/u.data')
+lines = sc.textFile('file:///C:/Users/freddygpa/Documents/Courses/pyspark/github/Pyspark/Scripts01/data/ratings.data')
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
